c36b0cf403e241245fa542d6daa26cb46eb55ae1
Garth McRae <garthmcrae@gmail.com>
2019-04-02 17:31:11 +1100

    Cross browser testing and amends to resolve issues.

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Shape from 'react-responsive-irregular-shapes';
 
 class App extends Component {
@@ -11,22 +11,19 @@ class App extends Component {
               background="darkslategray"
               color="white"
               insetBottom={`right`}
-              padding={`2rem 1rem 4rem`}
+              padding={`16px`}
           >
             <div className="container">
               <h1>React responsive irregular shapes</h1>
-            </div> 
+            </div>
           </Shape>
         </header>
         <main className="main">
           <div className="container">
-            <h2>Installation</h2>
-            <p>Using npm.</p>
-            <p><code>npm install react-responsive-irregular-shapes --save</code></p>
             <h2>Features</h2>
             <div className="layout">
               <div className="cell">
-                <Shape inset={`right top`} padding={`.5rem`}>
+                <Shape inset={`right top`} padding={`16`}>
                   <div className="margin">
                     <h3>Responsive</h3>
                     <p>Shapes maintain irregularity when increasing and decresing in size on both X and Y axis.</p>
@@ -34,7 +31,7 @@ class App extends Component {
                 </Shape>
               </div>
               <div className="cell">
-                <Shape inset={`right bottom`} padding={`.5rem`}>
+                <Shape inset={`right bottom`} padding={`16`}>
                   <div className="margin">
                     <h3>Irregular</h3>
                     <p>Configurable padding and insets for individual sides or pass shorthand values (see below).</p>
@@ -42,8 +39,8 @@ class App extends Component {
                 </Shape>
               </div>
               <div className="cell">
-                <Shape inset={`left bottom`} padding={`.5rem`}>
-                  <div className="margin">
+                <Shape inset={`left bottom`} padding={`16`}>
+                    <div className="margin">
                     <h3>Shapes</h3>
                     <p>Easily configure quadrilaterals, trapezia (trapezoids) and parrallelograms.</p>
                   </div>
@@ -51,79 +48,79 @@ class App extends Component {
               </div>
             </div>
           </div>
-          <Shape insetBottom={`right`} insetTop={`right`} padding={`3rem 0`}>
+          <Shape insetBottom={`right`} insetTop={`right`} padding={`16 0`}>
             <div className="container">
-              <h2>Usage</h2>
-              <p>Examples below of usage.</p>
-              <h3>Shorthand</h3>
-              <pre><code>
-&lt;Shape<br />
-&nbsp;&nbsp;background="darkslategray"<br />
-&nbsp;&nbsp;color="lightskyblue"<br />
-&nbsp;&nbsp;insetBottom="right"<br />
-&nbsp;&nbsp;padding="2rem 1rem 4rem"<br />
-&gt;<br />
-&nbsp;&nbsp;...<br />
-&lt;/Shape&gt;<br />
-              </code></pre>
-              <h3>Long hand</h3>
-              <pre><code>
-&lt;Shape<br />
-&nbsp;&nbsp;background="darkslategray"<br />
-&nbsp;&nbsp;color="lightskyblue"<br />
-&nbsp;&nbsp;insetBottom="right"<br />
-&nbsp;&nbsp;padding="2rem 1rem 4rem"<br />
-&gt;<br />
-&nbsp;&nbsp;...<br />
-&lt;/Shape&gt;<br />
-              </code></pre>
-              <h3>Mixed</h3>
-              <pre><code>
-&lt;Shape<br />
-&nbsp;&nbsp;background="darkslategray"<br />
-&nbsp;&nbsp;color="lightskyblue"<br />
-&nbsp;&nbsp;insetBottom="right"<br />
-&nbsp;&nbsp;padding="2rem 1rem 4rem"<br />
-&gt;<br />
-&nbsp;&nbsp;...<br />
-&lt;/Shape&gt;<br />
-              </code></pre>
+              <h2>Installation</h2>
+              <p>Using npm.</p>
+              <p><code>npm install react-responsive-irregular-shapes --save</code></p>
             </div>
           </Shape>
           <div className="container">
-            <h2>Properties</h2>
-            <p><code>
-              background = `whitesmoke`,<br />
-              children,<br />
-              color = `darkslategray`,<br />
-              className = `Shape`,<br />
-              display = `block`,<br />
-              fill = `white`,<br />
-              inset = `1rem`,<br />
-              insetTop = inset,<br />
-              insetRight = inset,<br />
-              insetBottom = insetTop,<br />
-              insetLeft = insetRight,<br />
-              padding = `1rem`,<br />
-              paddingTop = padding,<br />
-              paddingRight = padding,<br />
-              paddingBottom = paddingBottom,<br />
-              paddingLeft = paddingRight,<br />
-              position = `relative`,<br />
-            </code></p>
+            <h2>Usage</h2>
+            <p>Examples below of usage.</p>
+            <h3>Shorthand</h3>
+            <pre><code>
+  &lt;Shape<br />
+  &nbsp;&nbsp;background="darkslategray"<br />
+  &nbsp;&nbsp;color="lightskyblue"<br />
+  &nbsp;&nbsp;insetBottom="right"<br />
+  &nbsp;&nbsp;padding="16"<br />
+  &gt;<br />
+  &nbsp;&nbsp;...<br />
+  &lt;/Shape&gt;<br />
+            </code></pre>
+            <h3>Long hand</h3>
+            <pre><code>
+  &lt;Shape<br />
+  &nbsp;&nbsp;background="darkslategray"<br />
+  &nbsp;&nbsp;color="lightskyblue"<br />
+  &nbsp;&nbsp;insetBottom="right"<br />
+  &nbsp;&nbsp;padding="16"<br />
+  &gt;<br />
+  &nbsp;&nbsp;...<br />
+  &lt;/Shape&gt;<br />
+            </code></pre>
+            <h3>Mixed</h3>
+            <pre><code>
+  &lt;Shape<br />
+  &nbsp;&nbsp;background="darkslategray"<br />
+  &nbsp;&nbsp;color="lightskyblue"<br />
+  &nbsp;&nbsp;insetBottom="right"<br />
+  &nbsp;&nbsp;padding="16"<br />
+  &gt;<br />
+  &nbsp;&nbsp;...<br />
+  &lt;/Shape&gt;<br />
+            </code></pre>
           </div>
-        </main>
-        <footer className="footer">
-          <Shape
-              background="darkslategray"
-              color="white"
-              insetTop={`right`}
-              padding={`2rem 1rem 1rem 1rem`}
-          >
+          <Shape insetBottom={`right`} insetTop={`right`} padding={`16 0`}>
             <div className="container">
-              <p><a href="https://opensource.org/licenses/MIT">MIT licence</a> . <a href="https://github.com/garthmcrae/react-responsive-irregular-shapes">Github</a></p>
+              <h2>Properties</h2>
+              <p><code>
+                background = `whitesmoke`,<br />
+                children,<br />
+                color = `darkslategray`,<br />
+                className = `Shape`,<br />
+                display = `block`,<br />
+                fill = `white`,<br />
+                inset = `1rem`,<br />
+                insetTop = inset,<br />
+                insetRight = inset,<br />
+                insetBottom = insetTop,<br />
+                insetLeft = insetRight,<br />
+                padding = `1rem`,<br />
+                paddingTop = padding,<br />
+                paddingRight = padding,<br />
+                paddingBottom = paddingBottom,<br />
+                paddingLeft = paddingRight,<br />
+                position = `relative`,<br />
+              </code></p>
             </div>
           </Shape>
+        </main>
+        <footer className="footer">
+          <div className="container">
+            <p><a href="https://opensource.org/licenses/MIT">MIT licence</a> . <a href="https://github.com/garthmcrae/react-responsive-irregular-shapes">Github</a></p>
+          </div>
         </footer>
       </div>
     );
--- src/Shape.js
+++ src/Shape.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const attributes = {
-  preserveAspectRatio: 'none',
-  viewBox: '0 0 16 16',
+  preserveAspectRatio: `none`,
+  transform: `translate3d(0,0,0)`,
+  viewBox: `0 0 16 16`,
 }
+
+const bleed = 0
+const bias = `100%`
 
 const T = ({ fill, paddingTop, insetTop }) => {
   return insetTop === '' ? null : (
     <svg
       height={ paddingTop }
-      style={{ left: 0, top: 0, position: 'absolute' }}
-      width="100%"
+      style={{ left: 0, right: 0, top: bleed, position: 'absolute' }}
+      width={bias}
       {...attributes}
     >
       <path d={`M0,0 L16,0 L${ insetTop === 'left' ? 0 : 16},16z`} fill={ fill }></path>
@@ -21,8 +25,8 @@ const T = ({ fill, paddingTop, insetTop }) => {
 const R = ({ fill, paddingRight, insetRight }) => {
   return insetRight === '' ? null : (
     <svg
-      height="100%"
-      style={{ right: 0, top: 0, position: 'absolute' }}
+      height={bias}
+      style={{ bottom: 0, right: bleed, top: 0, position: 'absolute' }}
       width={ paddingRight }
       {...attributes}
     >
@@ -35,8 +39,8 @@ const B = ({ fill, paddingBottom, insetBottom }) => {
   return insetBottom === '' ? null : (
     <svg
       height={ paddingBottom }
-      style={{ bottom: 0, left: 0, position: 'absolute' }}
-      width="100%"
+      style={{ bottom: bleed, left: 0, right: 0, position: 'absolute' }}
+      width={bias}
       {...attributes}
     >
       <path d={`M${ insetBottom === 'left' ? 0 : 16},0 L16,16 L0,16z`} fill={ fill }></path>
@@ -47,8 +51,8 @@ const B = ({ fill, paddingBottom, insetBottom }) => {
 const L = ({ fill, paddingLeft, insetLeft }) => {
   return insetLeft === '' ? null : (
     <svg
-      height="100%"
-      style={{ left: 0, top: 0, position: 'absolute' }}
+      height={bias}
+      style={{ bottom: 0, left: bleed, top: 0, position: 'absolute' }}
       width={ paddingLeft }
       {...attributes}
     >
@@ -70,9 +74,10 @@ export default (props) => {
     color = `darkslategray`,
     display = `block`,
     fill = `white`,
-    inset = `1rem`,
-    padding = `1rem`,
+    inset = `top left`,
+    padding = `16px`,
     position = `relative`,
+    transform = `translate3d(0,0,0)`,
   } = props
   const [it, ir, ib, il] = x(props.inset)
   const {
@@ -83,13 +88,13 @@ export default (props) => {
   } = props
   const [pt, pr, pb, pl] = x(props.padding)
   const {
-    paddingTop = pt,
-    paddingRight = pr,
-    paddingBottom = pb,
-    paddingLeft = pl,
+    paddingTop = parseInt(pt, 10),
+    paddingRight = parseInt(pr, 10),
+    paddingBottom = parseInt(pb, 10),
+    paddingLeft = parseInt(pl, 10),
   } = props
   return (
-    <span style={{ background, color, display, paddingBottom, paddingLeft, paddingRight, paddingTop, position }}>
+    <span className={className} style={{ background, color, display, paddingBottom, paddingLeft, paddingRight, paddingTop, position, transform }}>
       <T {...{ fill, insetTop, paddingTop}} />
       <R {...{ fill, insetRight, paddingRight}} />
       <B {...{ fill, insetBottom, paddingBottom}} />
